0c289d4a8829dfcc041a7aec4636f7608e3a8cdd
ITMetalHeadGuy <36777017+ITMetalHeadGuy@users.noreply.github.com>
2022-11-25 14:31:59 +0100

    Update test_gr132.py

--- test_gr132.py
+++ test_gr132.py
@@ -15,19 +15,112 @@ class TestMessage(unittest.TestCase):
     def tearDown(self):
         pika.BlockingConnection(pika.ConnectionParameters('localhost')).channel().stop_consuming()
         tracemalloc.stop()
-
-    def test_messageSame(self):
-        testList = []
-        #  generate a random list what we will send to the server
-        for y in range(0,random.randrange(2,10)):
-            testList.append(random.randrange(-1000000,1000000))
+    def basic_method(self, testList):
         #  convert this list to Json form
         JsonSend=json.dumps({'input':testList})
         #  send the Json file to the server witch will process the datas
         pika.BlockingConnection(
             pika.ConnectionParameters('localhost')).channel().basic_publish(
                 exchange='output', routing_key='', body=JsonSend)
+        return   list(json.loads(((pika.BlockingConnection(pika.ConnectionParameters('localhost')
+                ).channel().basic_get(queue='outputs', auto_ack=True)[2]).decode()))['output'])
+        
+    def test_messageSame0(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
         #  process check the data from the server is it equal to the right parameters
         self.assertListEqual(sorted(testList),
-            list(json.loads(((pika.BlockingConnection(pika.ConnectionParameters('localhost')
-                ).channel().basic_get(queue='outputs', auto_ack=True)[2]).decode()))['output']))
+            getmessage)
+            
+    def test_messageSame1(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame2(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame3(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame4(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame5(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame6(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame7(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame8(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
+            
+    def test_messageSame9(self):
+        testList = []
+        #  generate a random list what we will send to the server
+        for y in range(0,random.randrange(2,10)):
+            testList.append(random.randrange(-1000000,1000000))
+        getmessage=TestMessage.basic_method(self=self, testList=testList)
+        #  process check the data from the server is it equal to the right parameters
+        self.assertListEqual(sorted(testList),
+            getmessage)
